67f584469fa51c9b4278260e933190a339abcded
mtrop <mtropiano@gmail.com>
2015-12-08 00:58:41 -0500

    Updated copyright.

--- src/com/blackrook/ogl/enums/FogCoordinateType.java
+++ src/com/blackrook/ogl/enums/FogCoordinateType.java
@@ -1,10 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2014-2015 Black Rook Software
+ * Copyright (c) 2014, 2015 Black Rook Software
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser Public License v2.1
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
- ******************************************************************************/
+ *
+ * Contributors:
+ *     Matt Tropiano - initial API and implementation
+ *******************************************************************************/
 package com.blackrook.ogl.enums;
 
 import com.jogamp.opengl.GL2;
